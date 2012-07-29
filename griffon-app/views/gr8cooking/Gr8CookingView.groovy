6bbfba61e537b318eb00168d21593d7881c37721
Danno Ferrin <danno.ferrin@shemnon.com>
2012-07-28 23:29:50 -0600

    add slides to view

--- griffon-app/views/gr8cooking/Gr8CookingView.groovy
+++ griffon-app/views/gr8cooking/Gr8CookingView.groovy
@@ -1,15 +1,54 @@
 package gr8cooking
 
 import javafx.stage.StageStyle
+import groovyx.javafx.GroovyFX
+import javafx.animation.Animation
+import com.sun.org.apache.xpath.internal.NodeSet
 
-def slides = []
 
 def slideNames = [
         Title,
         AboutMe,
+        Agenda,
+
+        JavaFX1,
+        JavaFX2,
+        GroovyFXSlide,
+
+        JavaFX2BigPoints,
+        SceneGraph,
+        Nodes,
+        LayoutNodes,
+
+        PropertiesAndBinding,
+
+        Controls,
+
+        CSSStyling,
+
+        AnimationSlide,
+
+        Effects,
+
+        MediaSlide,
+
+        Web,
+
+        FXMLSlide,
+
+        Tooling,
+
+        LegacyInterop,
+
+        QnA,
+        Thanks,
+
 ]
+def slides = []
+
 
 slideNames.each { slideName ->
+    println "slide - $slideName"
     if (app.views.containsKey(slideName)) {
         slides << app.views[slideName]
     } else {
