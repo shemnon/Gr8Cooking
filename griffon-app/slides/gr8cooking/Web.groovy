eecf7d750f79277f8936c187d7d69fe66d564acf
Danno Ferrin <danno.ferrin@shemnon.com>
2012-07-30 22:55:50 -0500

    Make web view wider

--- griffon-app/slides/gr8cooking/Web.groovy
+++ griffon-app/slides/gr8cooking/Web.groovy
@@ -7,7 +7,7 @@ stackPane(styleClass: 'slide', maxWidth: 640, maxHeight: 480, width: 640, height
         hbox vgrow: 'always', padding:[20, 20, 20, 20], {
             label vgrow: 'always', styleClass: "content-content", hgrow: 'always',
 """WebView Embeds a WebKit Powered HTML Renderer"""
-            web = webView minWidth: 300, prefWidth: 300, maxWidth: 300, minHeight: 300, prefHeight: 300, maxHeight: 300
+            web = webView minWidth: 420, prefWidth: 420, maxWidth: 420, minHeight: 300, prefHeight: 300, maxHeight: 300
             web.engine.load('http://gr8conf.us/')
         }
     }
